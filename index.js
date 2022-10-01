2dd1b2ac766fc3e850d30bb4870c511a565978e8
Jinda <neil_jnda@outlook.com>
2022-10-01 04:16:30 +0300

    ignore

--- index.js
+++ index.js
@@ -1,98 +1,83 @@
-const { response } = require('express')
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-//const cors = require('cors')
-const Person = require('./models/phonebook')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const Person = require('./models/phonebook');
 
+const app = express();
 
-//Middlewares in order
-//(1). Cross-Origin Resource Sharing for testing frontend and backend in diffrent ports
-//app.use(cors())
-//1. Use the imported frontend build
-app.use(express.static('build'))
-//2. Json parser
-app.use(express.json())
-//3. logger
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :detail'))
-morgan.token('detail', function (req, res) { return JSON.stringify(req.body)})
+//  Middlewares in order
+//  1. Use the imported frontend build
+app.use(express.static('build'));
+//  2. Json parser
+app.use(express.json());
+//  3. logger
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :detail'));
+morgan.token('detail', (req) => JSON.stringify(req.body));
 
-
-//Deal with http request
-app.get('/', (request, response) => {
-    response.send('<h1>Hi!</h1>')
-})
-app.get('/api/persons', (request, response) => {
+//  Deal with http request
+app.get('/', (request, response) => { response.send('<h1>Hi!</h1>'); });
+app.get('/api/persons', (request, response, next) => {
     Person.find({})
-        .then(persons =>{
-            //console.log(persons)
-            response.json(persons)
+        .then((persons) => {
+            //  console.log(persons)
+            response.json(persons);
         })
-        .catch(error => next(error))
-})
+        .catch((error) => next(error));
+});
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        if(person){
-            console.log(person)
-            response.json(person)
-        }
-        else{
-            console.log("404")
-            response.status(404).end()
+    Person.findById(request.params.id).then((person) => {
+        if (person) {
+            console.log('person: %d', person);
+            response.json(person);
+        } else {
+            console.log('404');
+            response.status(404).end();
         }
     })
-    .catch(error => next(error))
-})
+        .catch((error) => next(error));
+});
 app.put('/api/persons/:id', (request, response, next) => {
-    console.log(`Find by name ${body.name}and update`)
-    const body = request.body
-    Person.findByIdAndUpdate(request.params.id,{ $set: {number: body.number}} )
-    .then(person => {
-        if(person){
-            console.log(person)
-            response.json(person)
-        }
-        else{
-            console.log("404")
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
-})
+    const { body } = request;
+    Person.findByIdAndUpdate(request.params.id, { $set: { number: body.number } })
+        .then((person) => {
+            if (person) {
+                console.log(person);
+                response.json(person);
+            } else {
+                console.log('404');
+                response.status(404).end();
+            }
+        })
+        .catch((error) => next(error));
+});
 app.get('/info', (request, response) => {
-    Person.countDocuments({}, function (err, count) {
-        if (err){
-            console.log(err)
-            response.status(404).end()
-        }else{
-            console.log("Count :", count)
-            response.write(`<p>Phonebook has info for ${count} people</p>`)
-            response.write(`<p>${new Date().toString() }</p>`)
-            response.end()
+    Person.countDocuments({}, (err, count) => {
+        if (err) {
+            console.log(err);
+            response.status(404).end();
+        } else {
+            console.log('Count :', count);
+            response.write(`<p>Phonebook has info for ${count} people</p>`);
+            response.write(`<p>${new Date().toString()}</p>`);
+            response.end();
         }
     });
-})
-app.delete('/api/persons/:id', (request, response) => {
+});
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
+        .then(() => {
+            response.status(204).end();
         })
-        .catch(error => next(error))
-    response.status(204).end()
-})
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    //console.log(body)
+        .catch((error) => next(error));
+    response.status(204).end();
+});
+app.post('/api/persons', (request, response, next) => {
+    const { body } = request;
+    // console.log(body)
     if (!body) {
-      return response.status(400).json({ 
-        error: 'Content missing' 
-      })
-    }
-    else if(!body.name){
-        return response.status(400).json({ 
-            error: 'Name missing' 
-        })
+        return response.status(400).json({
+            error: 'Content missing',
+        });
     }
 
     // Checking for the same name is handled in frontend code
@@ -101,50 +86,47 @@ app.post('/api/persons', (request, response) => {
     //     response.json({"name" : body.name}, {number: body.number})
     // }
 
-    else if(!body.number){
-        return response.status(400).json({ 
-            error: 'Number missing' 
-        })
-    }
-    else{
-        //Create a new document from the model
-        console.log(`Create new: ${body.name}`)
-        const newPerson = new Person({
-            name: body.name,
-            number: body.number
-        })
-        
-        newPerson.save().then(savedPerson => {
-            response.json(savedPerson)
-        })
-    }
+    // Create a new document from the model
+    console.log(`Create new: ${body.name}`);
+    const newPerson = new Person({
+        name: body.name,
+        number: body.number,
+    });
 
-  })
+    newPerson.save().then((savedPerson) => {
+        response.json(savedPerson);
+    })
+        .catch((error) => next(error));
+    return null;
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
 
-
-//4. Unknown endpoint
+// 4. Unknown endpoint
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-  
-  // handler of requests with unknown endpoint
-app.use(unknownEndpoint)
+    response.status(404).send({ error: 'unknown endpoint' });
+};
 
-//5. error handler
-//When call next() with a parameter "error", this middleware is called
-const errorHandler = (err, req, res, next) => {
-    console.error("ErrorHandler", err.name)
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
 
-    if(err.name === 'CastError'){
-        return response.status(400).send({ error: 'malformatted id' })
+// 5. error handler
+// When call next() with a parameter "error", this middleware is called
+const errorHandler = (error, request, response, next) => {
+    console.error('ErrorHandler', error.name);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
     }
-    //Pass to default error handler of Express
-    //Note that the error handling middleware has to be the last loaded middleware!
-    next(err)
-}
-app.use(errorHandler)
+    if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
+    }
+    // Pass to default error handler of Express
+    // Note that the error handling middleware has to be the last loaded middleware!
+    next(error);
+    return null;
+};
+app.use(errorHandler);
--- models/phonebook.js
+++ models/phonebook.js
@@ -1,29 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URL
+const url = process.env.MONGODB_URL;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
+    .then(() => {
+        console.log('connected to MongoDB');
     })
-    .catch((error) =>{
-        console.log('connection failed: ', error)
-    })
-
+    .catch((error) => {
+        console.log('connection failed: ', error);
+    });
 
 const personSchema = mongoose.Schema({
-    name: String,
-    number: String
-})
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+    },
+    number: {
+        type: String,
+        minLength: 1,
+        required: true,
+    },
+});
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-})
-module.exports = mongoose.model('person', personSchema )
-  
-
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
+module.exports = mongoose.model('person', personSchema);
